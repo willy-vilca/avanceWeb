2dc4f4745a5f218cf668b46334b8290f91fae273
willy <willyvilca06@gmail.com>
2025-09-18 14:52:50 -0500

    first commit

---
+++ javaScript/index.js
@@ -0,0 +1,16 @@
+
+//Funcion para agregar o disminuir la cantidad de productos en las tarjetas
+function updateQty(btn, change) {
+      const input = btn.parentNode.querySelector("input");
+      let value = parseInt(input.value) || 1;
+      value += change;
+      if (value < 1) value = 1;
+      input.value = value;
+}
+
+//funcion para el menú desplegable de productos
+document.querySelectorAll('.dropdown-menu [data-bs-toggle="dropdown"]').forEach(function(element) {
+    element.addEventListener('click', function(e) {
+      e.stopPropagation();
+    });
+  });
